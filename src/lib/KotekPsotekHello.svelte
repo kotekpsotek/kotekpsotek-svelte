6f3c69078e4346db74078a4fd80aea978875fd68
kotekpsotek <siematumichael@protonmail.com>
2023-11-05 19:05:16 +0100

    kotekpsotek-hello is straight forward Hello text writed in svelte

---
+++ src/lib/KotekPsotekHello.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let afterSentence = "";
+</script>
+
+<p>
+    Hello to you from
+    <a href="https://github.com/kotekpsotek">kotekpsotek</a>!
+    {afterSentence}
+</p>
